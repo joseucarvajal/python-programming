add133a7ef6e0ee7cf698bc276279e3b9d5dbd19
joseucarvajal <joseucarvajal@gmail.com>
2024-08-14 19:26:03 -0500

    first commit

---
+++ v1.var.cte.operad.in.out.py
@@ -0,0 +1,4 @@
+edad_persona_1 = float(input("Ingrese la edad de la persona 1: "))
+edad_persona_2 = float(input("Ingrese la edad de la persona 2: "))
+edad_total = edad_persona_1 + edad_persona_2
+print(f"La edad sumada de las dos personas es: {edad_total}")
